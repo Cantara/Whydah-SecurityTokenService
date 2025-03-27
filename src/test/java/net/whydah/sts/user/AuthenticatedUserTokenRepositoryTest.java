7b5d55b3874e28b69c4058d06decdc67ef049f94
totto <totto@totto.org>
2025-03-27 12:43:36 +0100

    initial java 21+ update

--- src/test/java/net/whydah/sts/user/AuthenticatedUserTokenRepositoryTest.java
+++ src/test/java/net/whydah/sts/user/AuthenticatedUserTokenRepositoryTest.java
@@ -2,40 +2,39 @@ package net.whydah.sts.user;
 
 import net.whydah.sso.config.ApplicationMode;
 import net.whydah.sts.config.AppConfig;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class AuthenticatedUserTokenRepositoryTest {
+class AuthenticatedUserTokenRepositoryTest {
 
     private AppConfig appConfig;
 
-    @BeforeClass
-    public static void shared() {
+    @BeforeAll
+    static void shared() {
         Map<String, String> envs = new HashMap<>();
         envs.put(ApplicationMode.IAM_MODE_KEY, ApplicationMode.DEV);
         EnvHelper.setEnv(envs);
-
     }
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    void setUp() {
         appConfig = mock(AppConfig.class);
-
     }
 
     @Test
-    public void updateDefaultUserSessionExtensionTime() {
-        when(appConfig.getProperty(eq("user.session.timeout"))).thenReturn("240000");
+    void updateDefaultUserSessionExtensionTime() {
+        // Use the public method instead of the private one
+        when(appConfig.getProperty("user.session.timeout")).thenReturn("240000");
         long extensionSeconds = AuthenticatedUserTokenRepository.updateDefaultUserSessionExtensionTime(appConfig);
-        assertEquals(extensionSeconds, 240000L);
+        assertEquals(240000L, extensionSeconds);
     }
 }
